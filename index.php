f8b8ccb2a2bc923a1adcc7a2f5cf449474b3781d
Simon Grn <simon.garny@gmail.com>
2020-08-25 13:15:25 +0200

    add better visualization of selected labels

--- index.php
+++ index.php
@@ -116,6 +116,11 @@ $colors = [
             margin-top: 15px;
             margin-bottom: 10px;
         }
+
+        .type_selected {
+            font-weight: bold !important;
+            text-decoration: underline !important;
+        }
     </style>
 
     <title>PrestaShop Issues Stats</title>
@@ -173,7 +178,7 @@ $colors = [
                 foreach($labels as $type_id => $type_labels) {
             ?>
                 <div class="tab-pane <?php echo $active; ?>" id="<?php echo 'tab_label_'.$type_id; ?>" role="tabpanel" aria-labelledby="<?php echo $type_id; ?>-tab">
-                    <h6>Select/Unselect all
+                    <small>Select/Unselect all the labels :
                         <span class="type_icon type_icon_check_all" title="Check all labels for this type" data-type="<?php echo $type_id; ?>">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
@@ -185,7 +190,7 @@ $colors = [
                               <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
                             </svg>
                         </span>
-                    </h6>
+                    </small><br />
                     <?php
                         foreach($type_labels as $label) {
                             $c = '';
@@ -197,7 +202,7 @@ $colors = [
                             echo '
                             <span class="badge '.$class_badge.'">
                                 <label for="label_'.$label['id'].'" style="margin-bottom:0;">
-                                    <input type="checkbox" id="label_'.$label['id'].'" name="label['.$label['id'].']" '.$c.' class="type_checkbox type_'.$type_id.'"/>
+                                    <input type="checkbox" id="label_'.$label['id'].'" name="label['.$label['id'].']" '.$c.' class="type_checkbox type_'.$type_id.'" data-type="'.$type_id.'"/>
                                     <span title="'.$label['description'].'">'.$label['name'].'</span>
                                 </label>
                             </span>
@@ -223,19 +228,38 @@ $colors = [
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script>
+    //check uncheck all
     $(".type_icon_check_all").click(function() {
       const type_id = $(this).data('type');
       $(".type_"+type_id).prop("checked", true).trigger('change');
     });
+
     $(".type_icon_uncheck_all").click(function() {
       const type_id = $(this).data('type');
       $(".type_"+type_id).prop("checked", false).trigger('change');
     });
+
+    //select unselect a label
     $('.type_checkbox').change(function() {
       if(this.checked) {
         $(this).parents('span.badge').removeClass('badge-light').addClass('badge-dark');
       } else {
         $(this).parents('span.badge').removeClass('badge-dark').addClass('badge-light');
+      }
+      const type_id = $(this).data('type');
+      const type_checkboxes = $('.type_'+type_id);
+      let type_checked = false;
+      for(var i=0; i < type_checkboxes.length;  i++) {
+          let cur = $(type_checkboxes[i]);
+          if (cur.prop('checked')) {
+              type_checked = true;
+          }
+      }
+      console.log(type_checked);
+      if (type_checked) {
+          $("#label-"+type_id+"-tab").addClass('type_selected');
+      } else {
+          $("#label-"+type_id+"-tab").removeClass('type_selected');
       }
     });
 
